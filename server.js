b2496abdc6553b5deeb91e76d9db30ec00e92adc
BySokRaT <bysokrat@gmail.com>
2026-04-01 08:55:48 +0300

    git pushpassword login added

--- server.js
+++ server.js
@@ -1,16 +1,49 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const session = require("express-session");
 
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+  secret: "ikisatir-chat-secret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+function requireLogin(req, res, next) {
+  if (req.session.loggedIn) {
+    return next();
+  }
+  res.redirect("/login");
+}
+
+app.get("/login", (req, res) => {
+  res.send(`
+    <form method="POST" action="/login" style="text-align:center;margin-top:100px;">
+      <h2>İkisatır Giriş</h2>
+      <input type="password" name="password" placeholder="Şifre">
+      <button type="submit">Giriş</button>
+    </form>
+  `);
+});
+
+app.post("/login", (req, res) => {
+  if (req.body.password === "123456") {
+    req.session.loggedIn = true;
+    return res.redirect("/");
+  }
+  res.send("Şifre yanlış");
+});
+
+app.use("/", requireLogin, express.static("public"));
 
 const FILE = path.join(__dirname, "messages.json");
 
-// varsa eski mesajları yükle
 let messages = [];
 if (fs.existsSync(FILE)) {
   messages = JSON.parse(fs.readFileSync(FILE, "utf8"));
@@ -23,19 +56,13 @@ io.on("connection", (socket) => {
 
   socket.on("chat message", (msg) => {
     messages.push(msg);
-
-    // dosyaya kaydet
     fs.writeFileSync(FILE, JSON.stringify(messages, null, 2));
-
     io.emit("chat message", msg);
   });
 
   socket.on("delete message", (id) => {
     messages = messages.filter(m => m.id !== id);
-
-    // silince dosyayı güncelle
     fs.writeFileSync(FILE, JSON.stringify(messages, null, 2));
-
     io.emit("delete message", id);
   });
 });
